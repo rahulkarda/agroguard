8e7dd48aa188f3fdbfa29c2dfbe232dadf4ecc18
Rahul <rahulkarda2002@gmail.com>
2023-11-04 18:16:39 +0530

    initial commit

---
+++ web/src/config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "AgroGuard",
+  url: "https://AgroGuard.vercel.app",
+  ogImage: "https://AgroGuard.vercel.app/og.jpg",
+  description:
+    "Your digital gateway to the botanical world. Unearth a wealth of insights, from species details to disease detection, effortlessly bridging the gap between technology and the beauty of nature.",
+  links: {
+    twitter: "https://twitter.com/rahulkarda2002",
+    github: "https://github.com/rahulkarda/AgroGuard",
+  },
+}
+
+export type SiteConfig = typeof siteConfig
